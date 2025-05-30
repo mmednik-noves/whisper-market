29d2d3a5ba11ea4553cac2691ba36c17961746bd
mmednik-noves <martin@noves.fi>
2025-05-30 19:46:21 -0300

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
